abef1b70fa4c94d1124568bb434cb2f92bb36c22
Broderick Arneson <broderic@cs.ualberta.ca>
2006-11-29 03:13:28 +0000

    - removed Move() constructor - removed Move.toString() methods

--- src/hexgui/hex/Move.java
+++ src/hexgui/hex/Move.java
@@ -7,18 +7,11 @@ package hexgui.hex;
 //----------------------------------------------------------------------------
 
 /** Move.
-    Contains a HexPoint and a HexColor.  To construct a swap move use the
-    appropriate HexPoint. 
+    Contains a HexPoint and a HexColor.  To construct a swap moves or
+    other special moves use the appropriate HexPoint. 
 */
 public final class Move
 {
-    /** Constructs a move with <code>null</code> move and color. */
-    public Move()
-    {
-	m_point = null;
-    	m_color = null;
-    }
-
     /** Constructs a move with the given point and color.
 	@param p location of move
 	@param c black or white.
@@ -43,22 +36,6 @@ public final class Move
     public HexColor getColor()
     {
 	return m_color;
-    }
-
-    /** Returns a string representation of the move.
-	@return String of the form "[color, point]".
-    */
-    public String toString()
-    {
-	return toString(m_point, m_color);
-    }
-
-    private static String toString(HexPoint p, HexColor c)
-    {
-	if (p == null || c == null) 
-	    return "[null]";
-
-	return "[" + c.toString() + ", " + p.toString() + "]";
     }
 
     private HexPoint m_point;
